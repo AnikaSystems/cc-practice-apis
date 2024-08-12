e6bf5c3b250478b1f8c54cd59f130266d87a0e34
tylervoland <tyler.m.voland@gmail.com>
2024-08-12 10:33:13 -0400

    updated caseID

--- cases/src/main/java/com/anikasystems/casemanagement/service/repository/CaseRepository.java
+++ cases/src/main/java/com/anikasystems/casemanagement/service/repository/CaseRepository.java
@@ -1,6 +1,5 @@
 package com.anikasystems.casemanagement.service.repository;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +8,5 @@ import com.anikasystems.casemanagement.service.model.Case;
 
 public interface CaseRepository extends JpaRepository<Case, Long> {
 
-  List<Case> findByCaseID(String ID);
+  List<Case> findBycase_ID(String ID);
 }
