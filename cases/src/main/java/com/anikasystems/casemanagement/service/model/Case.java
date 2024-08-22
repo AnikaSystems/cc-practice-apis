fd8472a75cd3a578697b06731fa84ccd3cd0c129
tmccarthy-omni <tmccarthy@omnifederal.com>
2024-08-22 15:37:12 -0400

    table name typo

--- cases/src/main/java/com/anikasystems/casemanagement/service/model/Case.java
+++ cases/src/main/java/com/anikasystems/casemanagement/service/model/Case.java
@@ -3,10 +3,15 @@ package com.anikasystems.casemanagement.service.model;
 import java.sql.Date;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "iterp_iview_case")
+@Table(name = "interp_iview_case")
 public class Case {
 
     @Id
